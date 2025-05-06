187b22051767dcc8f5a9cd095aad11c34bab9282
Alberto Fernández <albertofernandezfernandez01@gmail.com>
2025-05-06 18:24:16 +0000

    [Fix] Code Review

--- src/app.js
+++ src/app.js
@@ -1,32 +1,28 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 
-function randomExcuse() {
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-
-  const whoIndexRandom = Math.floor(Math.random() * who.length)
-  const actionIndexRandom = Math.floor(Math.random() * action.length)
-  const whatIndexRandom = Math.floor(Math.random() * what.length)
-  const whenIndexRandom = Math.floor(Math.random() * when.length)
-
-  const excuse = `${who[whoIndexRandom]} ${action[actionIndexRandom]} ${what[whatIndexRandom]} ${when[whenIndexRandom]}`
-
-  return excuse
-
-}
-
 window.onload = function () {
+  function randomExcuse() {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  const p = document.getElementById("excuse")
+    const whoIndexRandom = Math.floor(Math.random() * who.length)
+    const actionIndexRandom = Math.floor(Math.random() * action.length)
+    const whatIndexRandom = Math.floor(Math.random() * what.length)
+    const whenIndexRandom = Math.floor(Math.random() * when.length)
 
-  p.innerHTML = randomExcuse()
+    const excuse = `${who[whoIndexRandom]} ${action[actionIndexRandom]} ${what[whatIndexRandom]} ${when[whenIndexRandom]}`
+
+    return excuse
+  }
+
+  const randomExcuseGenerator = document.getElementById("excuse")
+
+  randomExcuseGenerator.innerHTML = randomExcuse()
 
 };
